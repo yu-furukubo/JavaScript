89427ed4a9e7e18c807c5fb9b8b03329f45ef062
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-08-25 06:52:51 +0000

    [finish]cp6

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,9 +1,8 @@
-$(function (){
-  $('.box1').slideDown(5000,function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(1000);
+$(function(){
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
   });
-});
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+})
