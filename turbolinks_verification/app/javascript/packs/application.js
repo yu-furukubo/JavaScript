8c3288a7fe5db1e3a0c4fc0d5e9059efe051e128
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-08-29 01:50:55 +0000

    [finish]cp15

--- turbolinks_verification/app/javascript/packs/application.js
+++ turbolinks_verification/app/javascript/packs/application.js
@@ -7,10 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'jquery'
 import "script.js"
-import Swiper from 'swiper/swiper-bundle.js';
-import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
